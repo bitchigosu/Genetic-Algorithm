1a668f23ffafb32043cdc835bbe85bacc34856db
bitchigosu <kostya96.08@mail.ru>
2019-04-27 22:16:53 +0300

    Добавьте файлы проекта.

---
+++ Assets/GeneticAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GeneticAlgorithm<T>
+{
+    public List<DNA<T>> Population { get; private set; }
+    public int Generation { get; private set; }
+    public float MutationRate;
+
+	public GeneticAlgorithm()
+	{
+
+	}
+}
