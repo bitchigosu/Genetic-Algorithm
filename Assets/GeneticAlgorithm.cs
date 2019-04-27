f65c4ab934497c8f2fe2638da666f50128cac2b3
bitchigosu <kostya96.08@mail.ru>
2019-04-27 22:34:25 +0300

    17:31 in video

--- Assets/GeneticAlgorithm.cs
+++ Assets/GeneticAlgorithm.cs
@@ -6,9 +6,35 @@ public class GeneticAlgorithm<T>
     public List<DNA<T>> Population { get; private set; }
     public int Generation { get; private set; }
     public float MutationRate;
+    private Random random;
 
-	public GeneticAlgorithm()
+	public GeneticAlgorithm(int populationSize,int dnaSize, Random random, Func<T> getRandomGene, Func<float, int> fitnessFunction
+        , float mutationRate = 0.01f)
 	{
+        Generation = 1;
+        MutationRate = mutationRate;
+        Population = new List<DNA<T>>();
+        this.random = random;
 
+        for (int i = 0; i < populationSize; i++)
+        {
+            Population.Add(new DNA<T>(dnaSize, random, getRandomGene, fitnessFunction, true));
+        }
 	}
+
+    public void NewGeneration()
+    {
+        if (Population.Count <= 0)
+        {
+            return;
+        }
+        CalculateFitness();
+        List<DNA<T>> newPopulation = new List<DNA<T>>();
+
+    }
+
+    private void CalculateFitness()
+    {
+        throw new NotImplementedException();
+    }
 }
